99e249f7b1f2044e975d2d16c08b2d37e6de9310
aragao-a <aaa10@cin.ufpe.br>
2025-07-24 09:55:36 -0300

    taigaui attempt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideEventPlugins(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
